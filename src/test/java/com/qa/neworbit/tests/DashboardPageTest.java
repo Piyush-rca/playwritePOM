055a8d34be1ccdea7065acbacd949f2ecf80a909
Piyush Mangal <piyush.grn@gmail.com>
2025-10-18 09:42:28 +0530

    disable test for dashboardpagetest

--- src/test/java/com/qa/neworbit/tests/DashboardPageTest.java
+++ src/test/java/com/qa/neworbit/tests/DashboardPageTest.java
@@ -8,21 +8,22 @@ import com.qa.neworbit.constants.AppConstans;
 
 public class DashboardPageTest extends Basetest{
 	
-	@Test(priority = 1)
+	//@Test(priority = 1)
 	public void NavigateToDashboardPage() throws InterruptedException {
 		dashboardpage = loginpage.navigatetodashboard(prop.getProperty("email"), prop.getProperty("password"));
 	}
 	
-	@Test(priority = 2)
+	//@Test(priority = 2)
 	public void TitleverifyDashboard() {
 		String actualtitle = dashboardpage.DashboardTitle();
 		System.out.println("Actaual title of dashboard page : " + actualtitle);
 		Assert.assertEquals(actualtitle, AppConstans.DASHBOARD_PAGE_TITLE);
 	}
 	
-	@Test(priority = 3)
+	//@Test(priority = 3)
 	public void Dosearch() {
 	Assert.assertTrue(dashboardpage.search(prop.getProperty("city"), prop.getProperty("day"), prop.getProperty("month") ,prop.getProperty("year"), prop.getProperty("nationality")));
 	}
 
 }
+
