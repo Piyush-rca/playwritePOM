7429bbbc80b810158105cf1d0931ed38acb6841b
Piyush Mangal <piyush.grn@gmail.com>
2026-01-07 08:08:10 +0530

    Update PlaywrightFactory.java

--- src/main/java/com/qa/neworbit/factory/PlaywrightFactory.java
+++ src/main/java/com/qa/neworbit/factory/PlaywrightFactory.java
@@ -73,7 +73,7 @@ public class PlaywrightFactory {
 			
 		case "chrome":
 			//browser = playwright.chromium().launch(new LaunchOptions().setChannel("chrome").setHeadless(false));
-			tlBrowser.set(getPlaywright().chromium().launch(new LaunchOptions().setChannel("chrome").setHeadless(true)));
+			tlBrowser.set(getPlaywright().chromium().launch(new LaunchOptions().setHeadless(true)));
 			break;
 			
 			
@@ -127,6 +127,7 @@ public class PlaywrightFactory {
 		}
 		
 }
+
 
 
 
